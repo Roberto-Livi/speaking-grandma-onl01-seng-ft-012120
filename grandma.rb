7aecef6defe8a217f983e66d4cc1bb3fae9f9a90
Roberto Livi <robertomlivi@outlook.com>
2020-01-22 03:53:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,12 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938"
+    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 
-speak_to_grandma("Hey grandma")
-speak_to_grandma("HI GRANDMA")
+speak_to_grandma("Hi Nana, how are you?")
+speak_to_grandma("WHAT DID YOU EAT TODAY?")
 speak_to_grandma("I LOVE YOU GRANDMA!")
